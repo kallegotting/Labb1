c24b317bdbea69e8c0ec6fd4d3b1efc8cf126f19
kallegotting <45394651+kallegotting@users.noreply.github.com>
2019-11-01 08:53:44 +0100

    Update Uppgift8.java

--- Uppgift8.java
+++ Uppgift8.java
@@ -1,148 +1,158 @@
-package Labb1GUI;
+package Labb1;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.util.ArrayList;
 
 public class Uppgift8 extends Application {
 
-    private ImageView tomte;
-    private double x_speed = 10;
-    private int width = 1800;
-    private int height = 893;
-    private int theSize = 100;
-
-
     public static void main(String[] args) {
         launch(args);
     }
-    ArrayList<Image> tomteSpring = new ArrayList<>(567);
 
     @Override
     public void start(Stage primaryStage) {
-        ArrayList<TOMTEFAR> tomtar = new ArrayList<>();
+
+        ArrayList<Image> tomteSpringer = new ArrayList<>();
+        tomteSpringer.add(new Image("file:images/Run (1).png"));
+        tomteSpringer.add(new Image("file:images/Run (2).png"));
+        tomteSpringer.add(new Image("file:images/Run (3).png"));
+        tomteSpringer.add(new Image("file:images/Run (4).png"));
+        tomteSpringer.add(new Image("file:images/Run (5).png"));
+        tomteSpringer.add(new Image("file:images/Run (6).png"));
+        tomteSpringer.add(new Image("file:images/Run (7).png"));
+        tomteSpringer.add(new Image("file:images/Run (8).png"));
+        tomteSpringer.add(new Image("file:images/Run (9).png"));
+        tomteSpringer.add(new Image("file:images/Run (10).png"));
+        tomteSpringer.add(new Image("file:images/Run (11).png"));
+
+        ImageView tomten = new ImageView();
+        tomten.setImage(tomteSpringer.get(0));
+        tomten.setFitHeight(170);
+        tomten.setFitWidth(170);
+        tomten.setX(90);
 
         Group root = new Group();
 
+        Image bg = new Image("file:images/BG.png");
+        ImageView imageView = new ImageView(bg);
+        imageView.setPreserveRatio(true);
 
-        for(int i = 0; i < 11; i++){
-            tomtar.add(new TOMTEFAR(tomtar));
-        }
-        HBox vb1 = new HBox();
-        //vb1.setAlignment(Pos.BASELINE_LEFT);
-        vb1.getChildren().addAll(tomtar);
+        Image left = new Image("file:images/1.png");
+        ImageView ivLeft = new ImageView(left);
+        ivLeft.setX(0);
+        ivLeft.setY(670);
 
+        Image center1 = new Image("file:images/2.png");
+        ImageView ivCenter1 = new ImageView(center1);
+        ivCenter1.setX(120);
+        ivCenter1.setY(670);
 
+        Image center2 = new Image("file:images/2.png");
+        ImageView ivCenter2 = new ImageView(center2);
+        ivCenter2.setX(240);
+        ivCenter2.setY(670);
 
-        tomteSpring.add(new Image("file:images/Run(1).png"));
-        tomteSpring.add(new Image("file:images/Run(2).png"));
-        tomteSpring.add(new Image("file:images/Run(3).png"));
-        tomteSpring.add(new Image("file:images/Run(4).png"));
-        tomteSpring.add(new Image("file:images/Run(5).png"));
-        tomteSpring.add(new Image("file:images/Run(6).png"));
-        tomteSpring.add(new Image("file:images/Run(7).png"));
-        tomteSpring.add(new Image("file:images/Run(8).png"));
-        tomteSpring.add(new Image("file:images/Run(9).png"));
-        tomteSpring.add(new Image("file:images/Run(10).png"));
-        tomteSpring.add(new Image("file:images/Run(11).png"));
+        Image center3 = new Image("file:images/2.png");
+        ImageView ivCenter3 = new ImageView(center3);
+        ivCenter3.setX(360);
+        ivCenter3.setY(670);
 
+        Image center4 = new Image("file:images/2.png");
+        ImageView ivCenter4 = new ImageView(center4);
+        ivCenter4.setX(480);
+        ivCenter4.setY(670);
 
+        Image center5 = new Image("file:images/2.png");
+        ImageView ivCenter5 = new ImageView(center5);
+        ivCenter5.setX(600);
+        ivCenter5.setY(670);
 
-        tomte = new ImageView(new Image("file:images/Run (1).png"));
-        tomte.setFitWidth(200);
-        tomte.setPreserveRatio(true);
+        Image center6 = new Image("file:images/2.png");
+        ImageView ivCenter6 = new ImageView(center6);
+        ivCenter6.setX(720);
+        ivCenter6.setY(670);
 
+        Image center7 = new Image("file:images/2.png");
+        ImageView ivCenter7 = new ImageView(center7);
+        ivCenter7.setX(840);
+        ivCenter7.setY(670);
 
+        Image center8 = new Image("file:images/2.png");
+        ImageView ivCenter8 = new ImageView(center8);
+        ivCenter8.setX(960);
+        ivCenter8.setY(670);
 
-        tomte.setX(100);
-        tomte.setY(630);
+        Image right = new Image("file:images/3.png");
+        ImageView ivRight = new ImageView(right);
+        ivRight.setX(1085);
+        ivRight.setY(670);
 
+        Image Snogubbe = new Image("file:images/SnowMan.png");
+        ImageView ivSnogubbe = new ImageView(Snogubbe);
+        ivSnogubbe.setX(5);
+        ivSnogubbe.setY(470);
 
-        Image bakgrund = new Image("file:images/BG.png");
-        ImageView theBakgrund = new ImageView(bakgrund);
+        Image Igloo = new Image("file:images/Igloo.png");
+        ImageView ivIgloo = new ImageView(Igloo);
+        ivIgloo.setX(950);
+        ivIgloo.setY(470);
 
+        root.getChildren().addAll(imageView, ivLeft, ivCenter1, ivCenter2, ivCenter3, ivCenter4, ivCenter5, ivCenter6, ivCenter7, ivCenter8, ivRight, ivSnogubbe, ivIgloo, tomten);
 
-
-        Image igloo = new Image("file:images/Igloo.png");
-        ImageView theIgloo = new ImageView(igloo);
-        theIgloo.setFitHeight(100);
-        theIgloo.setFitWidth(175);
-        theIgloo.setX(1620);
-        theIgloo.setY(650);
-
-        Image skylt = new Image("file:images/Sign_2.png");
-        ImageView theSkylt = new ImageView(skylt);
-        theSkylt.setY(660);
-
-        Image vanster = new Image("file:images/1.png");
-        ImageView theVanster = new ImageView(vanster);
-        theVanster.setFitWidth(625);
-        theVanster.setFitHeight(150);
-        theVanster.setX(0);
-        theVanster.setY(749);
-
-
-        Image mitten = new Image("file:images/2.png");
-        ImageView theMitten = new ImageView(mitten);
-        theMitten.setFitWidth(600);
-        theMitten.setFitHeight(150);
-        theMitten.setX(625);
-        theMitten.setY(749);
-
-        Image hoger = new Image("file:images/3.png");
-        ImageView theHoger = new ImageView(hoger);
-        theHoger.setFitWidth(600);
-        theHoger.setFitHeight(150);
-        theHoger.setX(1225);
-        theHoger.setY(749);
-
-
-        root.getChildren().addAll(theBakgrund, theIgloo, theSkylt, theVanster, theMitten, theHoger, vb1);
-        //root.getChildren().addAll(tomtar);
-
-        Scene scene = new Scene(root, 1800, 893);
-        primaryStage.setTitle("Tomten");
+        Scene scene = new Scene(root, 1210, 800);
         primaryStage.setScene(scene);
+        primaryStage.setTitle("Tomten springer som om det inte finns en morgondag!");
         primaryStage.show();
 
-        //KeyFrame k = new KeyFrame(Duration.millis(10), event -> {
-        //            for(TOMTEFAR t: tomtar){
-        //                t.move();
-        //            }
-        //        });
 
+        new AnimationTimer(){
+            boolean right = true;
+            int x = 0;
+            int y = 525;
 
-        KeyFrame k = new KeyFrame(Duration.millis(10), event -> {
-                    for (int i = 0; i < tomteSpring.size(); i++){
-                        tomte.setX(tomte.getX() + x_speed);
-                        if (tomte.getX() <= 0  || tomte.getX() >= width - theSize*2) {
-                            x_speed = - x_speed;
-                            tomte.setScaleX(-1);
-                        }
+            @Override
+            public void handle(long arg0) {
+                if (right){
+                    tomten.setScaleX(1);
+                    if (x <= 820)
+                        x += 5;
+                    else if (x >= 820)
+                        right = false;
+                    }else {
+                        tomten.setScaleX(-1);
+                        if (x >= 0)
+                            x -=5;
+                        else if (x <= 0)
+                            right = true;
 
                     }
 
-               });
-        Timeline t = new Timeline(k);
-        t.setCycleCount(Timeline.INDEFINITE);
-        t.play();
+                tomten.setTranslateX(x);
+                tomten.setTranslateY(y);
+
+                }
+            } .start();
+
+        new AnimationTimer(){
+            int x;
+
+            @Override
+            public void handle(long arg0) {
+                x = (x + 1) % 11;
+                tomten.setImage(tomteSpringer.get(x));
+            }
+        } .start();
+
+
+
+
 
     }
 }
-
-//root.setBackground(new Background(new BackgroundImage(new Image("file:images/BG.png"), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
-//                BackgroundPosition.DEFAULT, new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false))));
-//        root.setAlignment(Pos.TOP_LEFT);
